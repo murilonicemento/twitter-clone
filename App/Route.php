d1989389342b1bba93902c5896649427da1ae07c
murilonicemento <murilo_nascimento22@outlook.com>
2023-10-06 17:02:01 -0300

    criação das rotas autenticar, timeline e sair

--- App/Route.php
+++ App/Route.php
@@ -19,10 +19,29 @@ class Route extends Bootstrap {
 			'controller' => 'indexController',
 			'action' => 'inscreverse'
 		);
+
 		$routes['registrar'] = array(
 			'route' => '/registrar',
 			'controller' => 'indexController',
 			'action' => 'registrar'
+		);
+
+		$routes['autenticar'] = array(
+			'route' => '/autenticar',
+			'controller' => 'AuthController',
+			'action' => 'autenticar'
+		);
+
+		$routes['timeline'] = array(
+			'route' => '/timeline',
+			'controller' => 'AppController',
+			'action' => 'timeline'
+		);
+
+		$routes['sair'] = array(
+			'route' => '/sair',
+			'controller' => 'AuthController',
+			'action' => 'sair'
 		);
 
 		$this->setRoutes($routes);
